0ffd8f73daf544b4210d627d4f0844c5fa020980
Verdier <ldverdier@gmail.com>
2024-01-16 19:57:29 +0100

    feat: add alphabet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,228 @@
 import styled from 'styled-components';
 
-const Text = styled.div<{ color?: string }>`
-  color: ${({ color }) => color ?? 'black'};
+const Header = () => {
+  return <h1>Alpha beta app is running well!</h1>;
+};
+
+type GreekLetter = {
+  name: string;
+  greekLowercase: string;
+  frenchLowercase: string;
+  greekUppercase: string;
+  frenchUppercase: string;
+};
+
+const GREEK_LOWERCASE_LETTERS: GreekLetter[] = [
+  {
+    name: 'alpha',
+    greekLowercase: 'α',
+    frenchLowercase: 'a',
+    greekUppercase: 'Α',
+    frenchUppercase: 'A',
+  },
+  {
+    name: 'bêta',
+    greekLowercase: 'β, ϐ',
+    frenchLowercase: 'b',
+    greekUppercase: 'Β',
+    frenchUppercase: 'B',
+  },
+  {
+    name: 'gamma',
+    greekLowercase: 'γ',
+    frenchLowercase: 'g',
+    greekUppercase: 'Γ',
+    frenchUppercase: 'G',
+  },
+  {
+    name: 'delta',
+    greekLowercase: 'δ',
+    frenchLowercase: 'd',
+    greekUppercase: 'Δ',
+    frenchUppercase: 'D',
+  },
+  {
+    name: 'epsilon',
+    greekLowercase: 'ε',
+    frenchLowercase: 'é',
+    greekUppercase: 'Ε',
+    frenchUppercase: 'E',
+  },
+  {
+    name: 'dzeta',
+    greekLowercase: 'ζ',
+    frenchLowercase: 'dz',
+    greekUppercase: 'Ζ',
+    frenchUppercase: 'Z',
+  },
+  {
+    name: 'eta',
+    greekLowercase: 'η',
+    frenchLowercase: 'è',
+    greekUppercase: 'Η',
+    frenchUppercase: 'È',
+  },
+  {
+    name: 'thêta',
+    greekLowercase: 'θ',
+    frenchLowercase: 'th',
+    greekUppercase: 'Θ',
+    frenchUppercase: 'Th',
+  },
+  {
+    name: 'iota',
+    greekLowercase: 'ι',
+    frenchLowercase: 'i',
+    greekUppercase: 'Ι',
+    frenchUppercase: 'I',
+  },
+  {
+    name: 'kappa',
+    greekLowercase: 'κ',
+    frenchLowercase: 'k',
+    greekUppercase: 'Κ',
+    frenchUppercase: 'K',
+  },
+  {
+    name: 'lambda',
+    greekLowercase: 'λ',
+    frenchLowercase: 'l',
+    greekUppercase: 'Λ',
+    frenchUppercase: 'L',
+  },
+  {
+    name: 'mu',
+    greekLowercase: 'μ',
+    frenchLowercase: 'm',
+    greekUppercase: 'Μ',
+    frenchUppercase: 'M',
+  },
+  {
+    name: 'nu',
+    greekLowercase: 'ν',
+    frenchLowercase: 'n',
+    greekUppercase: 'Ν',
+    frenchUppercase: 'N',
+  },
+  {
+    name: 'xi',
+    greekLowercase: 'ξ',
+    frenchLowercase: 'x',
+    greekUppercase: 'Ξ',
+    frenchUppercase: 'X',
+  },
+  {
+    name: 'omicron',
+    greekLowercase: 'ο',
+    frenchLowercase: 'o',
+    greekUppercase: 'Ο',
+    frenchUppercase: 'O',
+  },
+  {
+    name: 'pi',
+    greekLowercase: 'π',
+    frenchLowercase: 'p',
+    greekUppercase: 'Π',
+    frenchUppercase: 'P',
+  },
+  {
+    name: 'rho',
+    greekLowercase: 'ρ',
+    frenchLowercase: 'r',
+    greekUppercase: 'Ρ',
+    frenchUppercase: 'R',
+  },
+  {
+    name: 'sigma',
+    greekLowercase: 'σ, 𝛓',
+    frenchLowercase: 's',
+    greekUppercase: 'Σ',
+    frenchUppercase: 'S',
+  },
+  {
+    name: 'tau',
+    greekLowercase: 'τ',
+    frenchLowercase: 't',
+    greekUppercase: 'Τ',
+    frenchUppercase: 'T',
+  },
+  {
+    name: 'upsilon',
+    greekLowercase: 'υ',
+    frenchLowercase: 'u, y',
+    greekUppercase: 'Υ',
+    frenchUppercase: 'U, Y',
+  },
+  {
+    name: 'phi',
+    greekLowercase: 'φ',
+    frenchLowercase: 'ph',
+    greekUppercase: 'Φ',
+    frenchUppercase: 'Ph',
+  },
+  {
+    name: 'khi',
+    greekLowercase: 'χ',
+    frenchLowercase: 'ch',
+    greekUppercase: 'Χ',
+    frenchUppercase: 'Ch',
+  },
+  {
+    name: 'psi',
+    greekLowercase: 'ψ',
+    frenchLowercase: 'ps',
+    greekUppercase: 'Ψ',
+    frenchUppercase: 'Ps',
+  },
+  {
+    name: 'omega',
+    greekLowercase: 'ω',
+    frenchLowercase: 'o',
+    greekUppercase: 'Ω',
+    frenchUppercase: 'O',
+  },
+];
+
+const Cell = styled.th`
+  border: 1px black solid;
+  padding: 4px;
 `;
 
+const Alphabet = () => {
+  return (
+    <table>
+      <tbody>
+        {GREEK_LOWERCASE_LETTERS.map(
+          ({
+            name,
+            greekLowercase,
+            frenchLowercase,
+            greekUppercase,
+            frenchUppercase,
+          }) => {
+            return (
+              <tr>
+                <Cell>{name}</Cell>
+                <Cell>{greekLowercase}</Cell>
+                <Cell>{frenchLowercase}</Cell>
+                <Cell>{greekUppercase}</Cell>
+                <Cell>{frenchUppercase}</Cell>
+              </tr>
+            );
+          },
+        )}
+      </tbody>
+    </table>
+  );
+};
+
 function App() {
-  return <Text color="red">Alpha beta app is running well!</Text>;
+  return (
+    <>
+      <Header />
+      <Alphabet />
+    </>
+  );
 }
 
 export default App;
