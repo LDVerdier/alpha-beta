579dd83974b7629bf51dbd4b45f7368b9038e4bb
Verdier <ldverdier@gmail.com>
2024-01-16 19:14:45 +0100

    feat: add styling for testing purposes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import styled from 'styled-components';
+
+const Text = styled.div<{ color?: string }>`
+  color: ${({ color }) => color ?? 'black'};
+`;
+
 function App() {
-  return <div>Alpha beta app is running well!</div>;
+  return <Text color="red">Alpha beta app is running well!</Text>;
 }
 
 export default App;
