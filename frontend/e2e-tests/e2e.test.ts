0933ef9ee6d5e6ca30ae01d5f2553ab21c99976b
Verdier <ldverdier@gmail.com>
2024-01-19 23:21:48 +0100

    ci: add e2e test command

--- frontend/e2e-tests/e2e.test.ts
+++ frontend/e2e-tests/e2e.test.ts
@@ -1,5 +1,15 @@
-describe('describe', () => {
-  it('should', () => {
-    expect(true).toBe(true);
+import { Builder } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/chrome';
+
+describe('homepage', () => {
+  it('should display welcome message', async () => {
+    const driver = await new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(new Options().headless())
+      .build();
+    await driver.get('http://localhost:5173/');
+    const title = await driver.getTitle();
+
+    expect(title).toBe('Alpha Beta');
   });
 });
