b53fef3d1ca89173bc2a736c913cc4fbfc47b278
Verdier <ldverdier@gmail.com>
2024-01-19 20:49:04 +0100

    test: configure jest

--- frontend/jest.unit-config.ts
+++ frontend/jest.unit-config.ts
@@ -1,8 +1,13 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/styleMock.js',
   },
   preset: 'ts-jest',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
+  testRegex: '.test.(ts|tsx)$',
 };
+
+export default config;
