c5c38a71ed13a6bb076abe5807c6b3b3b4e2fc75
Laurent <ldverdier@gmail.com>
2024-02-19 21:20:10 +0100

    feat: add basic backend

--- frontend/src/HomePage/index.tsx
+++ frontend/src/HomePage/index.tsx
@@ -1,3 +1,13 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 export const HomePage = () => {
+  useEffect(() => {
+    (async () => {
+      const helloWorld = await axios.get(import.meta.env.VITE_BACKEND_URL);
+      console.info(helloWorld.data);
+    })();
+  }, []);
+
   return <div>Bienvenue sur Alpha Beta !</div>;
 };
