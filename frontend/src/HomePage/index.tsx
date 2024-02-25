48c103910b03d3061fe75da8ffce3f2aaf19f4ed
Laurent <ldverdier@gmail.com>
2024-02-25 12:27:07 +0100

    chore: move env variable to .env file

--- frontend/src/HomePage/index.tsx
+++ frontend/src/HomePage/index.tsx
@@ -4,8 +4,6 @@ import { useEffect } from 'react';
 export const HomePage = () => {
   useEffect(() => {
     (async () => {
-      console.info('vite env', import.meta.env);
-      console.info('vite backend url', import.meta.env.VITE_BACKEND_URL);
       const helloWorld = await axios.get(import.meta.env.VITE_BACKEND_URL);
       console.info(helloWorld.data);
     })();
